9df7e68a886d07a4322ed307f224018aca47b1ae
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:37:28 +0530

    Update srff_tb.v

--- day2/SRflipflop/Testbench/srff_tb.v
+++ day2/SRflipflop/Testbench/srff_tb.v
@@ -1,1 +1,32 @@
+module srff_tb(
 
+    );
+    reg s_tb,r_tb,rst_tb,clk_tb;
+    wire q_tb,qbar_tb;
+    srff dut(s_tb,r_tb,rst_tb,clk_tb,q_tb,qbar_tb);
+    
+    initial
+    begin
+    {s_tb,r_tb,rst_tb,clk_tb}=0;
+    end
+    always #5 clk_tb= ~ clk_tb;
+    initial
+    begin
+    rst_tb=1;
+    #10;
+    rst_tb=0;
+    s_tb=0;
+    r_tb=0;
+    #10;
+    s_tb=0;
+    r_tb=1;
+    #10;
+    s_tb=1;
+    r_tb=0;
+    #10;
+    s_tb=1;
+    r_tb=1;
+    
+    end
+    
+endmodule
