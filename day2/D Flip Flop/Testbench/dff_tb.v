cbdede3fc00a981b3fc721a4a34200accfd98d65
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:41:02 +0530

    Update dff_tb.v

--- day2/D Flip Flop/Testbench/dff_tb.v
+++ day2/D Flip Flop/Testbench/dff_tb.v
@@ -1,1 +1,29 @@
+module dff_tb(
+
+    );
+    reg d_tb,rst_tb,clk_tb;
+    wire q_tb,qbar_tb;
+    dff dut(d_tb,rst_tb,clk_tb,q_tb,qbar_tb);
+    initial
+    begin
+    {d_tb,rst_tb,clk_tb}=0;
+    end 
+    always #5 clk_tb= ~ clk_tb;
+    initial
+    begin
+    rst_tb=1;
+    #10;
+    rst_tb=0;
+    d_tb=1;
+    #10;
+    d_tb=0;
+    #10;
+    d_tb=1;
+    #10;
+    end
+    
+    
+    
+    
+endmodule
 
