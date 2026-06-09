0c0aa32503f036416cd8b21708ca9553a659adc3
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:36:27 +0530

    Update srff.v

--- day2/SRflipflop/Design/srff.v
+++ day2/SRflipflop/Design/srff.v
@@ -1,1 +1,31 @@
+module srff(input s,r,rst,clk, output reg q,qbar
 
+    );
+    always @(posedge clk) begin
+        if (rst) begin
+            q<=1'b0;
+            qbar<=1'b1; 
+        end
+        else begin
+            case({s,r})
+                2'b00: begin
+                q<=q;
+                qbar<=qbar;
+                end 
+                2'b01: begin
+                q<=1'b0;
+                qbar<=1'b1;
+                end 
+                2'b10: begin
+                q<=1'b1;
+                qbar<=1'b0;
+                end 
+                2'b11: begin
+                q<='bx;
+                qbar<='bx;
+                end 
+                
+            endcase
+        end
+    end
+endmodule
