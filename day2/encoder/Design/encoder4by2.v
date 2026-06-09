2ef8f4b7dd54ba5ff4c64b51c148219761b8252d
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:29:23 +0530

    Update encoder4by2.v

--- day2/encoder/Design/encoder4by2.v
+++ day2/encoder/Design/encoder4by2.v
@@ -1,1 +1,14 @@
+module encoder4by2(input [3:0]D,output reg[1:0]b
+
+    );
+    always @(*)
+    begin
+    case(D)
+    4'b0001:b=2'b00;
+    4'b0010:b=2'b01;
+    4'b0100:b=2'b10;
+    4'b1000:b=2'b11;
+    endcase 
+    end
+endmodule
 
