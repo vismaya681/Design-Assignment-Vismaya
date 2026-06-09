9607445622be16e4224f5e88dfcc1712f6398239
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:34:18 +0530

    Update encoder4by2_tb.v

--- day2/encoder/Testbench/encoder4by2_tb.v
+++ day2/encoder/Testbench/encoder4by2_tb.v
@@ -1,1 +1,26 @@
+module encoder4by2_tb(
+
+    );
+    reg [3:0]D_tb;
+    wire [1:0]b_tb;
+    
+    encoder4by2 dut(D_tb,b_tb);
+    initial 
+    begin
+    {D_tb}=0;
+    end
+    initial 
+    begin
+    D_tb=4'b0001;
+    #1;
+    D_tb=4'b0010;
+    #1;
+    D_tb=4'b0100;
+    #1;
+    D_tb=4'b1000;
+    #1;
+    
+    end 
+    
+endmodule
 
