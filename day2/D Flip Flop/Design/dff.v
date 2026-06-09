6d751eb1fa5c2ea59b1bcdb619618cafad582770
vismaya681 <vismayakv2306@gmail.com>
2026-06-09 20:39:28 +0530

    Update dff.v

--- day2/D Flip Flop/Design/dff.v
+++ day2/D Flip Flop/Design/dff.v
@@ -1,1 +1,15 @@
+module dff(input d,rst,clk,output reg q,qbar
+
+    );
+    always @(posedge clk) begin
+        if (rst) begin
+            q<=1'b0;
+            qbar<=1'b1; 
+        end
+        else begin
+            q<=d;
+            qbar<=~d;
+        end  
+    end 
+endmodule
 
