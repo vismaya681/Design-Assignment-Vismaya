f13898a5e7ded473c9c024aac6868b63ab361cfe
vismaya681 <vismayakv2306@gmail.com>
2026-06-10 21:07:55 +0530

    Update out.v

--- day3/Face detection/Design/out.v
+++ day3/Face detection/Design/out.v
@@ -1,1 +1,54 @@
 
+
+module out(
+    input clk,
+    input rst,
+    output reg rd_enb
+);
+
+parameter S0 = 2'b00;
+parameter S1 = 2'b01;
+parameter S2 = 2'b10;
+
+reg [1:0] state;
+
+always @(posedge clk)
+begin
+    if(rst)
+    begin
+        state <= S0;
+        rd_enb <= 1'b0;
+    end
+    else
+    begin
+        case(state)
+
+        S0:
+        begin
+            rd_enb <= 1'b0;
+            state <= S1;
+        end
+
+        S1:
+        begin
+            rd_enb <= 1'b0;
+            state <= S2;
+        end
+
+        S2:
+        begin
+            rd_enb <= 1'b1;
+            state <= S0;
+        end
+
+        default:
+        begin
+            rd_enb <= 1'b0;
+            state <= S0;
+        end
+
+        endcase
+    end
+end
+
+endmodule
