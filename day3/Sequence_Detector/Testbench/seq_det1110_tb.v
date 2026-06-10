c2d140739d2ab1a3cda215b4ed25694f1ef9d10a
vismaya681 <vismayakv2306@gmail.com>
2026-06-10 20:21:00 +0530

    Update seq_det1110_tb.v

--- day3/Sequence_Detector/Testbench/seq_det1110_tb.v
+++ day3/Sequence_Detector/Testbench/seq_det1110_tb.v
@@ -1,1 +1,33 @@
+module seq_det1110_tb(
+
+    );
+    reg clk_tb,rst_tb,din_tb;
+    wire detected_tb;
+    
+    seq_det1110 dut(clk_tb,rst_tb,din_tb,detected_tb);
+    
+    initial
+    begin
+    {clk_tb,rst_tb,din_tb}=0;
+    end
+    
+    always #5 clk_tb=~clk_tb;
+    
+    initial
+    begin
+    rst_tb=1;
+    #10;
+    rst_tb=0;
+    
+    
+    din_tb=1;
+    #10;
+    din_tb=1;
+    #10;
+    din_tb=1;
+    #10;
+    din_tb=0;
+    #10;
+    end
+endmodule
 
