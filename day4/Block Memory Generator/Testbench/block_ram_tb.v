9867b9c35fa8835a2db4ff0b78457e76cbb70b50
vismaya681 <vismayakv2306@gmail.com>
2026-06-11 18:33:54 +0530

    Update block_ram_tb.v

--- day4/Block Memory Generator/Testbench/block_ram_tb.v
+++ day4/Block Memory Generator/Testbench/block_ram_tb.v
@@ -1,1 +1,51 @@
+module block_ram_tb(
 
+    );
+    reg clk_tb,arstn_tb,wrenb_tb;
+    reg [7:0] wr_address_tb;
+    reg [7:0] rd_address_tb;
+    reg [7:0] data_in_tb;
+    wire [7:0] data_out_tb;
+    
+    block_ram dut(clk_tb,arstn_tb,wrenb_tb,wr_address_tb,rd_address_tb,data_in_tb,data_out_tb);
+    
+    initial 
+    begin
+    {clk_tb,arstn_tb,wrenb_tb,wr_address_tb,rd_address_tb,data_in_tb}=0;
+    end
+    
+    always #5 clk_tb=~clk_tb;
+    
+    initial
+    begin
+    #20;
+    arstn_tb=1;
+    #10;
+    
+    //writing data
+    wrenb_tb=1;
+    wr_address_tb=8'h05;
+    data_in_tb=8'hAA;
+    #10;
+    
+    wr_address_tb=8'h2A;
+    data_in_tb=8'h4B;
+    #10;
+    
+    //reading data
+    wrenb_tb=0;
+    rd_address_tb=8'h05;
+    #10;
+    
+    rd_address_tb=8'h2A;
+    #10;
+    
+    //asynchronous reset
+    #5;
+    arstn_tb=0;
+    #10;
+    
+    
+    
+    end
+endmodule
