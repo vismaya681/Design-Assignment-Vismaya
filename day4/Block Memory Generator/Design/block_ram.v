3bb29bef67dbe17e5568270eddae1c791107b920
vismaya681 <vismayakv2306@gmail.com>
2026-06-11 18:32:27 +0530

    Update block_ram.v

--- day4/Block Memory Generator/Design/block_ram.v
+++ day4/Block Memory Generator/Design/block_ram.v
@@ -1,1 +1,19 @@
+module block_ram(input clk,arstn,wrenb,input [7:0]wr_address,input [7:0]rd_address,input [7:0]data_in,output reg [7:0]data_out
 
+    );
+    reg [7:0] memory[0:255];
+    always @(posedge clk or negedge arstn) begin
+        if (!arstn) begin
+            // Asynchronous active-low reset
+            data_out <= 8'h00;
+        end else begin
+            if (wrenb == 1'b1) begin
+                // Write Operation
+                memory[wr_address] <= data_in;
+            end else begin
+                // Read Operation (wrenb == 0)
+                data_out <= memory[rd_address];
+            end
+        end
+    end
+endmodule
