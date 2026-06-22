07ab65d88d69462560ddea5db7ff68ecab635ab4
vismaya681 <vismayakv2306@gmail.com>
2026-06-22 16:22:20 +0530

    Add APB interface definition in interface.sv

--- day8/AMBA APB Protocol Verification/verification/interface.sv
+++ day8/AMBA APB Protocol Verification/verification/interface.sv
@@ -1,1 +1,10 @@
-
+interface apb_if(input logic pclk, input logic presetn);
+    logic [31:0] paddr;
+    logic        psel;
+    logic        penable;
+    logic        pwrite;
+    logic [31:0] pwdata;
+    logic        pready;
+    logic        pslverr;
+    logic [31:0] prdata;
+endinterface
