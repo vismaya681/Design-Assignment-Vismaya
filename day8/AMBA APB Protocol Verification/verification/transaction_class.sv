f9f56ff5b6cf91091c1d3832e4e07e822930350e
vismaya681 <vismayakv2306@gmail.com>
2026-06-22 16:25:38 +0530

    Update transaction_class.sv

--- day8/AMBA APB Protocol Verification/verification/transaction_class.sv
+++ day8/AMBA APB Protocol Verification/verification/transaction_class.sv
@@ -1,1 +1,19 @@
+class apb_transaction;
+    rand bit [31:0] paddr;
+    rand bit [31:0] pwdata;
+    rand bit        pwrite;
+    
+    // Outputs from the DUT
+    bit [31:0] prdata;
+    bit        pslverr;
 
+    // Constraints to test both valid (0-31) and out-of-bounds addresses (>31)
+    constraint addr_c {
+        paddr dist { [0:31] :/ 80, [32:100] :/ 20 }; 
+    }
+
+    function void display(string name);
+        $display("[%s] Addr: %0d, Write: %0b, WData: %0h, RData: %0h, SlvErr: %0b", 
+                 name, paddr, pwrite, pwdata, prdata, pslverr);
+    endfunction
+endclass
