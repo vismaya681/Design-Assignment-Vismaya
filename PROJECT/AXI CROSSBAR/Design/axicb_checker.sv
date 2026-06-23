4be2bc2ea162aca26dd4ce56cdc21f58c6efc20e
vismaya681 <vismayakv2306@gmail.com>
2026-06-23 08:49:31 +0530

    Update axicb_checker.sv

--- PROJECT/AXI CROSSBAR/Design/axicb_checker.sv
+++ PROJECT/AXI CROSSBAR/Design/axicb_checker.sv
@@ -1,1 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.06.2026 12:41:04
+// Design Name: 
+// Module Name: axicb_checker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+`ifndef AXICB_CHECKERS
+`define AXICB_CHECKERS
+
+`define CHECKER(condition, msg)\
+    if (condition) begin \
+        $display("\033[1;31mERROR: %s\033[0m", msg); \
+        $finish(1); \
+    end
+
+`endif
